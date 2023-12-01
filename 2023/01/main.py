d1ca636b0f3a4826953c308321d5d07c5338799b
Jonatan Höst <jonatan.host@metabytes.se>
2023-12-01 13:54:02 +0100

    day two done

--- 2023/01/main.py
+++ 2023/01/main.py
@@ -1,62 +1,33 @@
 import re
 
-
 fileObject = open("input.txt", "r")
 Lines = fileObject.readlines()
-
-help_dict = {
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9',
-    'zero': '0'
-}
 
 codes = []
 sum = 0
 
-
-def extract_numbers(input_string):
-    """
-    Extracts and returns numbers (both in numeral and word form) from a given string in their original order.
-    """
-
-    # Dictionary to map number words to their numerical equivalents
-    number_words = {
-        "one": "1", 
-        "two": "2", 
-        "three": "3", 
-        "four": "4", 
-        "five": "5",
-        "six": "6", 
-        "seven": "7", 
-        "eight": "8", 
-        "nine": "9"
-    }
-
-    # Regular expression to match number words
-    number_word_regex = "|".join(number_words.keys())
-
-    # Extracting numbers (numerals and words)
-    numbers = re.findall(rf'\d+|{number_word_regex}', input_string, re.IGNORECASE)
-
-    # Replace number words with their numerical equivalents
-    separated_numbers = []
-    for number in numbers:
-        number = number_words.get(number.lower(), number)
-        separated_numbers.extend([digit for digit in number])
-
-    return separated_numbers
+number_words = [
+    ("one", "o1e"), 
+    ("two", "t2o"), 
+    ("three", "t3e"), 
+    ("four", "f4r"), 
+    ("five", "f5e"),
+    ("six", "s6x"), 
+    ("seven", "s7n"), 
+    ("eight", "e8t"), 
+    ("nine", "n9e"), 
+]
 
 
 for line in Lines:
 
-    filtered = extract_numbers(line)
+
+    for p in number_words:
+        line = line.replace(*p)
+    print(line)
+
+    filtered = [int(i) for i in line if i.isdigit()]
+    print(filtered)
 
     if len(filtered) == 1:
         filtered = int(str(filtered[0]) + str(filtered[0]))
@@ -66,7 +37,6 @@ for line in Lines:
         filtered = int(str(filtered[0]) + str(filtered[-1]))
         codes.append(filtered)
         sum += filtered
-
 
 print(codes)
 print("SUM: ", sum)
