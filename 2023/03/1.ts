214208eddc211de23dc56d168cc208302bca8427
Jonatan Höst <jonatan.h96@gmail.com>
2023-12-03 23:03:38 +0100

    Add function

--- 2023/03/1.ts
+++ 2023/03/1.ts
@@ -6,8 +6,26 @@ var sum: Number = 0
 var tempNr: String = ""
 var parts: Number[] = []
 
+
+var start: number[] = [0,0]
+var stop: number[] = [3,3]
+
+
 // Create 2D Array using split to separate each character
 const array: string[][] = lines.map(row => row.split(""));
+
+// Get coordinates and check for special character
+function findSpecial(start: number[], stop: number[]) {
+  for(var i = start[0]; i <= stop[0]; i++) {
+    var cube = array[i];
+    for(var j = start[1]; j <= stop[1]; j++) {
+      console.log(i,j, " ",cube[j])
+      return !/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(cube[j]);
+    }
+  }
+}
+var result = findSpecial([1,1],[3,4])
+
 
 
 for(var i = 0; i < array.length; i++) {
@@ -16,15 +34,20 @@ for(var i = 0; i < array.length; i++) {
       console.log("cube[" + i + "][" + j + "] = " + cube[j]);
 
       if(isNaN(cube[j])){
-        console.log("Not a number")
-        parts.push(parseInt(tempNr))
-        tempNr = ""
+        // Check if tempNr is not an empty string before parsing and pushing
+        if (tempNr !== "") {
+          const numberToAdd = parseInt(tempNr);
+          // Only push the number if it's not NaN
+          if (!isNaN(numberToAdd)) {
+            parts.push(numberToAdd);
+          }
+        }
+        tempNr = "";
       } else {
         tempNr += cube[j]
       }
-      console.log(tempNr)
   }
 }
 
-console.log("Sum: ", sum)
-console.log(parts)
+console.log("Result: ", result)
+console.log(parts);
