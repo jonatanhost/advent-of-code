5231bac0e00f86819f75eb8e72dad9effdbb02b5
Jonatan Höst <jonatan.h96@gmail.com>
2023-12-04 13:54:00 +0100

    almost done

--- 2023/03/1.ts
+++ 2023/03/1.ts
@@ -4,9 +4,8 @@ import { shrink } from "bun";
 const input = await Bun.file("input.txt").text();
 const lines = input.split(/\r?\n/);
 
-var sum: Number = 0;
 var tempNr: String = "";
-var parts: Number[] = [];
+var parts: number[] = [];
 
 var startPos: number[] = [0, 0];
 var stopPos: number[] = [3, 3];
@@ -14,16 +13,22 @@ var stopPos: number[] = [3, 3];
 // Create 2D Array using split to separate each character
 const array: string[][] = lines.map((row) => row.split(""));
 
+var results: boolean[] = []
+
 // Get coordinates and check for special character
 function findSpecial(start: number[], stop: number[]) {
-    console.log("Start: ", start, ", Stop: ", stop);
+    results = []
+    console.log("Start: ", start, " Stop: ", stop);
     for (var i = start[0]; i <= stop[0]; i++) {
-        var cube = array[i];
+        var symbols = array[i];
         for (var j = start[1]; j <= stop[1]; j++) {
-            console.log(i, j, " ", cube[j]);
-            return !/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(cube[j]);
+            console.log(i, j, ": ", symbols[j]);
+            results.push( /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(symbols[j]) )
         }
     }
+
+    var containsTrue = results.some(element => element === true);    
+    return containsTrue
 }
 
 for (var i = 0; i < array.length; i++) {
@@ -36,6 +41,7 @@ for (var i = 0; i < array.length; i++) {
             if (tempNr !== "") {
                 if (startPos) {
                     stopPos = [i + 1, j];
+                    stopPos = [Math.min(stopPos[0], 139), Math.min(stopPos[1], 139)];
                     var result = findSpecial(startPos, stopPos);
                     console.log(result);
                     if (result) {
@@ -53,11 +59,16 @@ for (var i = 0; i < array.length; i++) {
             }
             tempNr = "";
         } else {
-            startPos = [Math.max(0, i - 1), Math.max(0, j - 1)];
+            if(startPos.length === 0){
+                startPos = [Math.max(0, i - 1), Math.max(0, j - 1)];
+            }
             tempNr += cube[j];
         }
     }
 }
 
-console.log("Result: ", result);
-console.log(parts);
+const sum = parts.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+}, 0);
+
+console.log(sum); // Output will be the sum of the array elements
