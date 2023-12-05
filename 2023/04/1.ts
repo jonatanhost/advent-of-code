9eabb9977838589cf67f0e3b50e98bb01f6c23f9
Jonatan Höst <jonatan.host@metabytes.se>
2023-12-05 10:49:05 +0100

    day 4 started

--- 2023/04/1.ts
+++ 2023/04/1.ts
@@ -2,7 +2,23 @@
 const input = await Bun.file("input.txt").text();
 const lines = input.split(/\r?\n/);
 
-// Create 2D Array using split to separate each character
-const array: string[][] = lines.map((row) => row.split(""));
+var sum: number = 0;
 
-console.log("Hello via Bun!");
+const cardObjects = lines.map((line) => {
+    const [firstPart, secondPart] = line.split("|").map(
+        (part) => part.trim().split(/\s+/).slice(2).map(Number) // Removing "Card X:" part
+    );
+    return { card: firstPart, winners: secondPart };
+});
+
+cardObjects.forEach((element) => {
+    console.log("Game x");
+
+    console.log(element.card);
+    for (var card of element.card) {
+        let result = element.winners.includes(card);
+        console.log("Card: ", card, "Result: ", result);
+    }
+});
+
+console.log(cardObjects[0].card);
